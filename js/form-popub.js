956c66132575849629d6849c4285f8d8a7f17d25
Margo <margo.k1007@gmail.com>
2018-07-20 17:33:30 +0300

    plug JS for popub

--- js/form-popub.js
+++ js/form-popub.js
@@ -1,23 +1,23 @@
-// //  Работа с popub-form на главной
+//  Работа с popub-form на главной
 
-// var link = document.querySelector(".form-title-button");
-// var popub = document.querySelector(".hotel-search-form");     
-// // var buttonClose = document.querySelector(".modal-content .modal-content-close");
+var link = document.querySelector(".form-title-button");
+var popub = document.querySelector(".hotel-search-form");     
+var buttonClose = document.querySelector(".modal-content-close");
 
+    link.addEventListener("click", function(event) {
+        event.preventDefault();
+        popub.classList.add("hotel-search-form-show");
+    });
 
-//     link.addEventListener("click", function(event) {
-//         event.preventDefault();
-//         popub.classList.add("hotel-search-form-show");
+    buttonClose.addEventListener("click", function(event) {
+        event.preventDefault();
+        popub.classList.remove("hotel-search-form-show");
+    });
 
-
-// // Закрытие popum с помощью ESC
-
-//     window.addEventListener("keydown", function(event) {
-//         if (event.keyCode === 27) {
-//             if (popub.classList.contains("hotel-search-form-show")) {
-//                 popub.classList.remove("hotel-search-form-show");
-//             }
-//         }
-//     });
-
-
+    window.addEventListener("keydown", function(event) {
+        if (event.keyCode === 27) {
+            if (popub.classList.contains("hotel-search-form-show")) {
+                popub.classList.remove("hotel-search-form-show");
+            }
+        }
+    });
